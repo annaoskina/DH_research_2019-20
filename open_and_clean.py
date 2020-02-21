91e5115eac61f4f86b8c9e1425bb00f9c778d60c
Anna Oskina <annaoskina@gmail.com>
2020-02-21 19:06:57 +0300

    regexps works

--- open_and_clean.py
+++ open_and_clean.py
@@ -6,9 +6,9 @@ def open_the_file (filename):
       return raw_txt
 
 def clean_the_file (txt1):
-      step_1 = re.sub ('(----------).+(---------)', '', txt1) #очищаю от условных обозначений в начале
+      step_1 = re.sub ('(----------).+(---------)', '', txt1, flags=re.DOTALL) #очищаю от условных обозначений в начале
       step_2 = re.sub ('《.+》', '', step_1)  #очищаю от фуриганы
-      clean_txt = re.sub ('(底本：「).+(ボランティアの皆さんです。)', '', step_2) #очищаю от метаданных
+      clean_txt = re.sub ('(底本：「).+?(ボランティアの皆さんです。)', '', step_2, flags=re.DOTALL) #очищаю от метаданных
       return clean_txt
 
 def main():
