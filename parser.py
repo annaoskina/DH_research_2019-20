66046e708823776c68b79a37aad17c5451123c4d
Anna Oskina <annaoskina@gmail.com>
2020-03-09 18:23:42 +0300

    improved parser

--- parser.py
+++ parser.py
@@ -7,18 +7,21 @@ def open_the_file(filename):
       return raw_txt
 
 def clean_the_file(raw_file):
-      step_1 = re.sub ('(----------).+(---------)', '', raw_file, flags=re.DOTALL) #очищаю от условных обозначений в начале
-      step_2 = re.sub ('《.+?》', '', step_1)  #очищаю от фуриганы
-      clean_txt = re.sub ('(底本：「).+?(ボランティアの皆さんです。)', '', step_2, flags=re.DOTALL) #очищаю от метаданных
+      step_1 = re.sub('(----------).+(---------)', '', raw_file, flags=re.DOTALL) #очищаю от условных обозначений в начале
+      step_2 = re.sub('《.+?》', '', step_1)  #очищаю от фуриганы
+      step_3 = re.sub('※［＃「均のつくり」、第3水準1-14-75］', '', step_2)
+      clean_txt = re.sub('(底本：「).+?(ボランティアの皆さんです。)', '', step_3, flags=re.DOTALL) #очищаю от метаданных
       return clean_txt
 
 def parse_the_text(clean_file):
       m = MeCab.Tagger('-d /home/anna/Documents/UniDic-kindai_1603')
       parsed_txt = m.parse(clean_file)
+      #list_all_words = parsed_txt.split('\n')
+      #return list_all_words
       return parsed_txt
 
 def write_the_file(parsed_txt):
-      fw = open("parsed_txt.txt", "w", encoding = "utf-8")
+      fw = open("parsed.csv", "w", encoding = "utf-8")
       fw.write("{}".format(parsed_txt))
       fw.close()
 
@@ -26,6 +29,7 @@ def main():
       raw_file = open_the_file('Source_for_research/majutsu.txt')
       clean_file = clean_the_file(raw_file)
       parsed_txt = parse_the_text(clean_file)
+      print(parsed_txt)
       fw = write_the_file(parsed_txt)
 
 if __name__ == '__main__':
