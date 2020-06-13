bc2fcefaa3a8921d2d39ac7484ccc0dae869c224
Anna Oskina <annasokina@gmail.com>
2020-06-13 21:12:58 +0300

    mistake

--- Statistic.R
+++ Statistic.R
@@ -1,18 +1,18 @@
 library(tidyverse)
-df <- read_csv('https://raw.githubusercontent.com/annaoskina/DH_research_2019-20/master/Results_csv/kokoro.txt.csv', col_names = FALSE)
+df <- read_csv('https://raw.githubusercontent.com/annaoskina/DH_research_2019-20/master/Results_csv/kappa.txt.csv', col_names = FALSE)
 
 df %>% 
-  select(-X27) %>% 
-  pivot_longer(names_to = "cols", values_to = "values", X2:X13) %>% 
+  pivot_longer(names_to = "cols", values_to = "values", X2:X4) %>% 
   pivot_wider(names_from = X1, values_from = values) %>% 
   select(-cols) %>%
   cor()
 
 df %>% 
-  pivot_longer(names_to = "cols", values_to = "values", X2:X50) %>% 
+  pivot_longer(names_to = "cols", values_to = "values", X2:X4) %>% 
   pivot_wider(names_from = X1, values_from = values) %>% 
   select(-cols) %>% 
   ggplot(aes(katakana, kanji))+
+  #geom_smooth(method="lm")
   geom_point()
 
 df %>% 
