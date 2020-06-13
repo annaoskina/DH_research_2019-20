8f4026d7960f3b2bcdd928a08b3987fcd449a6fd
Anna Oskina <annasokina@gmail.com>
2020-06-13 20:01:27 +0300

    added correlation spread sheet

--- R/pirson.R
+++ R/pirson.R
@@ -1,24 +1,35 @@
 library(tidyverse)
 df <- read_tsv(library(tidyverse))
 df <- read_tsv("https://raw.githubusercontent.com/annaoskina/DH_research_2019_20/master/R/Correlation.tsv")
-
-df %>%
-  select(-Style) %>%   
+df1 <- read_csv("https://raw.githubusercontent.com/annaoskina/DH_research_2019_20/master/R/Correlation1.csv")
+df1 %>%
+  select(-Style) %>% 
   na.omit() %>% 
   mutate(Filename = str_c(1:n(), " ", Filename)) %>% 
   as.data.frame() ->
-  df
+  df1
 
-rownames(df) <- df$Filename
+rownames(df1) <- df1$Writing
 
-df %>% 
-  select(`katakana-romaji`, `romaji-kanji`, `kanji-katakana`) %>% 
+df1 %>% 
+  select(`katakana_romaji`, `romaji_kanji`, `kanji_katakana`) %>% 
   dist() %>% 
   hclust() %>% 
   plot()
 
-df %>% 
-  na.omit() %>% 
-  ggplot(aes('katakana-romaji', 'kanji-katakana', color = 'Name of author'))+
-  geom_point()
+df1 <- read_csv("https://raw.githubusercontent.com/annaoskina/DH_research_2019_20/master/R/Correlation1.csv")
 
+df1 %>% 
+  select(-Style) %>% 
+  na.omit() %>% 
+  ggplot(aes(katakana_romaji, kanji_katakana, color = Author, label = Writing))+
+  geom_point()+
+  geom_text()
+
+df1 %>% 
+  select(-Style) %>%
+  na.omit %>% 
+  ggplot(aes(fct_reorder(Writing, romaji_kanji), romaji_kanji))+
+  #ggplot(aes(Writing, katakana_romaji)) +
+  geom_col()+
+  coord_flip()
